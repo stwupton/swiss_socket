5206c16a42088582886dde8c23e56b5b0a16c53c
Steven Upton <supton.sz@gmail.com>
2016-09-07 11:00:44 +0100

    Fixed issue where defining a max in query parameter will create new socket pool.

--- lib/swiss_socket.ts
+++ lib/swiss_socket.ts
@@ -48,9 +48,26 @@ export class SocketServer {
 
       this.server.on('connection', (webSocket: WebSocket): void => {
 
+        // Get socket pool identifier and maximum sockets cap from query parameter.
+        let maxSockets: number = this.maxSockets;
+        let identifier: string = webSocket.upgradeReq.url;
+        if (identifier.search(new RegExp('\\?', 'g')) != -1) {
+
+          let urlSplit: string[] = identifier.split('?');
+          identifier = urlSplit[0];
+
+          let query: {} = this.getQueryParameters(urlSplit[1]);
+          if (query['max'] != null &&
+              query['max'].length > 0 &&
+              !isNaN(parseInt(query['max'])) &&
+              parseInt(query['max']) > 0)
+            maxSockets = parseInt(query['max']);
+
+        }
+
         // Get existing pool with identifier.
         let pools: SocketPool[] = this.socketPools.filter(
-            (pool: SocketPool) => pool.identifier == webSocket.upgradeReq.url);
+            (pool: SocketPool) => pool.identifier == identifier);
 
         let pool: SocketPool;
         if (pools.length > 0) {
@@ -61,22 +78,6 @@ export class SocketServer {
             webSocket.send(JSON.stringify({_ss_error: 'Maximum socket pools reached.'}));
             webSocket.close();
             return;
-          }
-
-          // Get socket pool identifier and maximum sockets cap from query parameter.
-          let maxSockets: number = this.maxSockets;
-          let identifier: string = webSocket.upgradeReq.url;
-          if (identifier.search(new RegExp('\\?', 'g')) != -1) {
-
-            let urlSplit: string[] = identifier.split('?');
-            identifier = urlSplit[0];
-
-            let query: {} = this.getQueryParameters(urlSplit[1]);
-            if (query['max'] != null &&
-                query['max'].length > 0 &&
-                !isNaN(parseInt(query['max'])))
-              maxSockets = parseInt(query['max']);
-
           }
 
           // Check that the 'max' parameter specifies does not exceed the maximum
@@ -92,13 +93,6 @@ export class SocketServer {
 
         }
 
-        let successful: boolean = pool.addSocket(webSocket);
-        if (!successful) {
-          webSocket.send(JSON.stringify({_ss_error: 'Maximum sockets reached.'}));
-          webSocket.close();
-          return;
-        }
-
         // Remove socket pool when all `WebSocket`'s have disconnected.
         webSocket.on('close', () => {
 
@@ -109,6 +103,13 @@ export class SocketServer {
             this.socketPools.splice(this.socketPools.indexOf(pool), 1);
 
         });
+
+        let successful: boolean = pool.addSocket(webSocket);
+        if (!successful) {
+          webSocket.send(JSON.stringify({_ss_error: 'Maximum sockets reached.'}));
+          webSocket.close();
+          return;
+        }
 
       });
 
